24abafa30135074553937b80c685c45d29e9ccbf
Tyler <Tyl3r.s@outlook.com>
2022-09-27 20:49:09 -0400

    add product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,8 +9,8 @@ router.get('/', (req, res) => {
     attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
     include: [
       {
-          model: Category,
-          attributes: ['id', 'category_name'],
+        model: Category,
+        attributes: ['id', 'category_name'],
       },
       {
         model: Tag,
@@ -24,3 +24,34 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     })
 });
+
+// get one product
+router.get('/:id', (req, res) => {
+  // find a single product by its `id`
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    // be sure to include its associated Category and Tag data
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product found with this id' })
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(500).json(err);
+    })
+});
+
+// create new product
