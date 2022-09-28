1bad91de1087d1a606ea7ffecdfc2dbcf66b9dca
Tyler <Tyl3r.s@outlook.com>
2022-09-27 22:06:21 -0400

    fix sequelize bug in product model

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,14 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-const { dateToString } = require('sqlstring');
-
 // import our database connection from config.js
 const sequelize = require('../config/connection');
-const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -28,15 +24,15 @@ Product.init(
       allowNull: false,
       validate: {
         isDecimal: true
-      }
-    },
+        }
+      },
     stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     defaultValue: 10,
+     validate: {
+       isNumeric: true
+     }
     },
     category_id: {
       type: DataTypes.INTEGER,
@@ -45,8 +41,6 @@ Product.init(
         key: 'id'
       }
     }
-  },
-  {
   },
   {
     sequelize,
